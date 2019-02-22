1ad01a28a25cde7d3fbf7ee15fa3ba5c544d81c3
tiepshopstack <47884233+tiepshopstack@users.noreply.github.com>
2019-02-22 16:17:12 +0700

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,15 +1,33 @@
 'use strict';
-class Point{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
 
-    plus(Point){
-        this.x += Point.x;
-        this.y += Point.y;
-        return this;
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Point =
+/*#__PURE__*/
+function () {
+  function Point(x, y) {
+    _classCallCheck(this, Point);
+
+    this.x = x;
+    this.y = y;
+  }
+
+  _createClass(Point, [{
+    key: "plus",
+    value: function plus(Point) {
+      this.x += Point.x;
+      this.y += Point.y;
+      return this;
     }
-}
+  }]);
+
+  return Point;
+}();
 
 console.log(new Point(1, 2).plus(new Point(2, 1)));
