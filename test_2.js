70217025f65543358e43661c45e82c6cc27776fc
tiepshopstack <47884233+tiepshopstack@users.noreply.github.com>
2019-02-22 16:17:48 +0700

    Update test_2.js

--- test_2.js
+++ test_2.js
@@ -1,27 +1,88 @@
 'use strict';
-class A{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
-    cong(){
-        return this.x + this.y
-    }
-}
 
-class B extends A {
-    tru(){
-        return this.x - this.y;
-    }
-}
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-class C extends B {
-    nhan(){
-        return this.x*this.y;
-    }
-}
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-const a = new C(20,10);
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var A =
+/*#__PURE__*/
+function () {
+  function A(x, y) {
+    _classCallCheck(this, A);
+
+    this.x = x;
+    this.y = y;
+  }
+
+  _createClass(A, [{
+    key: "cong",
+    value: function cong() {
+      return this.x + this.y;
+    }
+  }]);
+
+  return A;
+}();
+
+var B =
+/*#__PURE__*/
+function (_A) {
+  _inherits(B, _A);
+
+  function B() {
+    _classCallCheck(this, B);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(B).apply(this, arguments));
+  }
+
+  _createClass(B, [{
+    key: "tru",
+    value: function tru() {
+      return this.x - this.y;
+    }
+  }]);
+
+  return B;
+}(A);
+
+var C =
+/*#__PURE__*/
+function (_B) {
+  _inherits(C, _B);
+
+  function C() {
+    _classCallCheck(this, C);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(C).apply(this, arguments));
+  }
+
+  _createClass(C, [{
+    key: "nhan",
+    value: function nhan() {
+      return this.x * this.y;
+    }
+  }]);
+
+  return C;
+}(B);
+
+var a = new C(20, 10);
 console.log(a.cong());
 console.log(a.tru());
 console.log(a.nhan());
